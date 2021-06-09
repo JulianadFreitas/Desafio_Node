da7a4cb0c607b2d8793558aa9691385dea251d6c
Juliana <julianachristello@gmail.com>
2021-06-08 23:56:03 -0300

    ajustes

--- desafio.js
+++ desafio.js
@@ -1,37 +1,48 @@
 import readlineSync from "readline-sync";
 
-function TodoList() {
+    
     let tasks = [];
-    function renderList() {
-        for (let i = 0; i <= tasks.length; i++) {
-            console.log(tasks[i], "deu certo :D");
-        }
-    }
-
-    while (true) {
-
-        const options = ['add', 'list', 'remove'];
-        let index = readlineSync.keyInSelect(options, 'Which?');
+    const options = ['add', 'list', 'check', 'remove'];
+    let index = readlineSync.keyInSelect(options, 'Type your comand?');
+    while (index !== -1){
         if (index === 0) {
             const task = readlineSync.question('What do you want to do? ');
-            tasks.push(`🔴 ${task}`);
-            console.log(tasks);
+            const todoo = {text: task, done: false};
+            tasks.push(todoo);
         }
         if (index === 1) {
-            renderList()
-        }
-        if (index === 2) {
-            const check = readlineSync.keyInSelect(tasks, 'What to do do you want to check?');
-            console.log(array[check]);
-            array[check].splice(0, 1);
-        }
-        if (index === 3) {
-            const remove = readlineSync.keyInSelect(tasks, 'What to do do you want to remove?');
-            tasks.splice(remove, 1);
+            console.log("===========================");
+            if(tasks.length === 0)(console.log("nenhuma tarefa"));
+            tasks.forEach((item) => {
+            const emoji = item.done ? '🟢' : '🔴';
+            console.log(`${emoji} ${item.text}`)
+        });
+            console.log("===========================");
+        }else if (index === 2) {
+            const optionsCheck = tasks.map((item) => {
+               const emoji = item.done ? '🟢' : '🔴';
+               return `${emoji} ${item.text}`
+            });
+            
+            const IndexToCheck = readlineSync.keyInSelect(optionsCheck , 'What to do do you want to check?');
+            if (IndexToCheck  !== -1){
+                const todo = tasks[IndexToCheck];
+                todo.done = !todo.done;
+            } 
+        }else if (index === 3) {
+            const optionsCheck = tasks.map((item) => {
+               const emoji = item.done ? '🟢' : '🔴';
+               return `${emoji} ${item.text}`
+            });
+            
+            const IndexToRemove = readlineSync.keyInSelect(optionsCheck , 'What to do do you want to remove?');
 
-            renderList();
+            
+             tasks.splice(IndexToRemove, 1)
         }
-    }
-}
 
-TodoList();
+        index = readlineSync.keyInSelect(options, 'Type your comand?');
+    } 
+    console.log('fim');
+
+
